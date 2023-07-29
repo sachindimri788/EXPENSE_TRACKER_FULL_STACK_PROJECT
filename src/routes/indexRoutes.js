576f4fc7ceab3feab049ed182c5a60bec5b50e33
sachindimri788 <sachindimri788@gmail.com>
2023-07-29 16:55:54 +0530

    ready to deploy

--- src/routes/indexRoutes.js
+++ src/routes/indexRoutes.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const router = express.Router();
+const path = require("path");
 
 const userRoutes = require('./userRoutes');
-const expenseRoutes=require('./expenseRoute');
-const premiumRoutes=require('./premiumRoutes');
+const expenseRoutes = require('./expenseRoute');
+const premiumRoutes = require('./premiumRoutes');
+
+
+
 
 router.use('/user', userRoutes);
-router.use('/expense',expenseRoutes);
-router.use('/premium',premiumRoutes);
+router.use('/expense', expenseRoutes);
+router.use('/premium', premiumRoutes);
+
+router.use('/', (req, res) => {
+    res.sendFile(path.join(__dirname, `../views/${req.url}`))
+})
 
 module.exports = router;
